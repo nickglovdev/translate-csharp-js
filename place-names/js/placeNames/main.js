a4e2543a09bea807aa1a4b34fc089bf5ed917fcd
Nicholas Glover <nickglovdev@gmail.com>
2020-08-21 14:43:21 -0500

    Finished the first part of the assignment

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,27 @@
 function main() {
   // Put your code here
+  // Array of Locations
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+
+  console.log("All Place Names")
+
+  // Replace for foreach
+  for (let name of names) {
+    console.log(name)
+  }
+  // New line
+  console.log("\n")
+
+  const theNames = names.filter(name => name.includes("The"))
+
+  console.log("'The' Place Names")
+
+  for (let name of theNames) {
+    console.log(name)
+  }
 
 }
+
 
 main();
