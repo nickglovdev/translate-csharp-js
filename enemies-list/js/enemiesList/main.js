837e667efec62da64e3db48cd6de09ddfe5525b9
Nicholas Glover <nickglovdev@gmail.com>
2020-08-24 13:42:34 -0500

    Working on the second parts of questions

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,30 @@
 function main() {
-  // Put your code here
+  console.log("My Enemies List!")
+  console.log("----------------")
+
+  // const enemies = GetEnemies();
+
+  // for (let myEnemy of enemies) {
+  //   if (myEmemy.IsReallyHated) {
+  //     console.log(`${myEnemy.firstName} ${myEnemy.lastName} (Really, really dislike)`)
+  //   }
+  // }
+
+  const GetEnemies = (firstName, lastName, offenses, isReallyHated) => {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.addOffense = (offense) => {
+      offenses.push(offense)
+    },
+      this.isReallyHated = isReallyHated
+  }
+  let user = new GetEnemies("Joshua", "Flowers", [
+    "Being a jerk to me in elementary school",
+    "Not being nice to me in elementary school"
+  ],
+    true)
+  console.log(user)
+
 
 }
 
